41b3f686d60f68f1b8373976f96b91525afc5fde
AdairBento <adair.bento@hotmail.com>
2026-01-30 10:46:39 -0300

    fix(api): ignorar arquivos .d.ts no ESLint

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -14,11 +14,12 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules', '**/*.d.ts', '**/*.js', '**/*.map'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 };
