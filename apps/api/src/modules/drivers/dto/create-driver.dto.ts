52c368c1aac541026e2bc114385cb1639bd362a3
AdairBento <adair.bento@hotmail.com>
2026-01-31 12:41:10 -0300

    refactor: usar enums centralizados em CreateDriverDto

--- apps/api/src/modules/drivers/dto/create-driver.dto.ts
+++ apps/api/src/modules/drivers/dto/create-driver.dto.ts
@@ -1,5 +1,6 @@
-import { IsString, IsEmail, IsOptional, IsDateString } from 'class-validator';
+import { IsString, IsEmail, IsOptional, IsDateString, IsEnum } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { LicenseCategory } from '@mag-system/shared-types';
 
 export class CreateDriverDto {
   @ApiProperty()
@@ -22,9 +23,13 @@ export class CreateDriverDto {
   @IsString()
   licenseNumber: string;
 
-  @ApiProperty()
-  @IsString()
-  licenseCategory: string;
+  @ApiProperty({
+    enum: LicenseCategory,
+    example: LicenseCategory.B,
+    description: 'Driver license category'
+  })
+  @IsEnum(LicenseCategory)
+  licenseCategory: LicenseCategory;
 
   @ApiProperty()
   @IsDateString()
