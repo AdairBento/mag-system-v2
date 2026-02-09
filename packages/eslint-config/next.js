cf71021acc335a5864b3ff46333ec278a6062b5f
AdairBento <adair.bento@hotmail.com>
2026-02-09 01:20:02 -0300

    fix(eslint-config): remove duplicate @typescript-eslint from next preset
    The parser and plugin were already defined in base.js, causing a conflict
    when next.js extended base. Removed the duplicate declarations.

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -4,8 +4,6 @@ module.exports = {
     'next/core-web-vitals',
     'plugin:@typescript-eslint/recommended',
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-unused-vars': [
       'warn',
