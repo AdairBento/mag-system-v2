f3e10683993a84cb22d234010321de53770c89ee
Adair Bento <SEU_EMAIL_REAL_AQUI>
2026-02-15 23:33:00 -0300

    fix(lint): stabilize tsconfig project lint + avoid web eslint crash in lint-staged

---
+++ packages/eslint-config/index.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn'
+  },
+  ignorePatterns: ['dist', 'node_modules', '.turbo', 'coverage']
+};
